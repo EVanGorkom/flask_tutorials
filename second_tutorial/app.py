bafaaa85c3dc8b91a06773149eca3367914a8a7b
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-04-02 02:40:14 -0500

    Create session functionality and encryption for the session secret key

--- second_tutorial/app.py
+++ second_tutorial/app.py
@@ -1,8 +1,14 @@
 from flask import Flask, redirect, url_for, render_template, request
 from views import views
+from dotenv import load_dotenv
+import os
 
 app  = Flask(__name__)
 app.register_blueprint(views, url_prefix="")
+
+load_dotenv()
+secret_key = os.getenv('SECRET_KEY')
+app.secret_key = secret_key
 
 if __name__ == '__main__':
     app.run(debug=True, port=800)
--- second_tutorial/views.py
+++ second_tutorial/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for, render_template, request, decorator
+from flask import Blueprint, redirect, url_for, render_template, request, session
 
 views = Blueprint(__name__, "views")
 
@@ -10,10 +10,15 @@ def home():
 def login():
     if request.method == "POST":
         user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
+        session["user"] = user
+        return redirect(url_for("views.user"))
     else:
         return render_template("login.html")
 
-@views.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@views.route("/user")
+def user():
+    if "user" in session and session["user"] != "":
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("views.login"))
