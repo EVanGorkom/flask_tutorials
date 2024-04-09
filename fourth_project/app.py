f63bc44eacae4a783552c079cbef63d2983dedd0
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-04-08 22:52:11 -0500

    Create the 'average' endpoint using more SQL queries

--- fourth_project/app.py
+++ fourth_project/app.py
@@ -8,11 +8,15 @@ CREATE_ROOMS_TABLE =  (
     "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);"
 )
 CREATE_TEMPS_TABLE = """CREATE TABLE IF NOT EXISTS temperatures (room_id INTEGER, temperature REAL, 
-date TIMESTAMP, FOREIGN_KEY(room_id) REFERENCES rooms(id) ON DELETE CASCADE):"""
+date TIMESTAMP, FOREIGN KEY(room_id) REFERENCES rooms(id) ON DELETE CASCADE);"""
 
 INSERT_ROOM_RETURN_ID = "INSERT INTO rooms (name) VALUES (%s) RETURNING id;"
 INSERT_TEMP = (
     "INSERT INTO temperatures (room_id, temperature, date) VALUES (%s, %s, %s);"
+)
+
+GLOBAL_NUMBER_OF_DAYS = (
+    """SELECT COUNT(DISTINCT DATE(date)) AS days FROM temperatures;"""
 )
 GLOBAL_AVG = """SELECT AVG(temperature) as average FROM temperatures;"""
 
@@ -51,3 +55,12 @@ def add_temp():
 
     return {"message": "Temperature added."}, 201
 
+@app.get("/api/average")
+def get_global_avg():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(GLOBAL_AVG)
+            average = cursor.fetchone()[0]
+            cursor.execute(GLOBAL_NUMBER_OF_DAYS)
+            days = cursor.fetchone()[0]
+    return {"average": round(average, 2), "days": days}
