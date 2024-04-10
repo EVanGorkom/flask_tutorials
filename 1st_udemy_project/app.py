c9be47fd8d72a5ede477d251d997926cf3de267f
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-04-09 23:33:45 -0500

    Add the POST request to the application

--- 1st_udemy_project/app.py
+++ 1st_udemy_project/app.py
@@ -1,3 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+stores = [
+    {
+        "names": "My Store",
+        "items": [
+            {
+                "price": 15.99,
+                "name": "Chair"
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_store():
+    return {"stores": stores}
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
