8c0d0a574833a303841f668565e0919cf74953b5
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-04-15 11:31:32 -0500

    Reorganize the CRUD functions to be grouped by database 'class'

--- 1st_udemy_project/app.py
+++ 1st_udemy_project/app.py
@@ -5,7 +5,7 @@ from db import items, stores
 
 app = Flask(__name__)
 
-
+# Store CRUD
 @app.get("/store")
 def get_store():
     return {"stores": list(stores.values())}
@@ -25,6 +25,27 @@ def create_store():
     store = {**store_data, "id": store_id}
     stores[store_id] = store
     return store, 201
+
+@app.get("/store/<string:store_id>")
+def get_specific_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found")
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "store deleted."}
+    except KeyError:
+        abort(404, message="store not found.")
+        
+
+# Item CRUD
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
 
 @app.post("/item")
 def create_item(name):
@@ -53,40 +74,12 @@ def create_item(name):
     items[item_id] = item
     return item, 201
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-@app.get("/store/<string:store_id>")
-def get_specific_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        abort(404, message="Store not found")
-
-
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
         abort(404, message="Store not found")
-
-@app.delete("/item/<string:item_id>")
-def delete_item(item_id):
-    try:
-        del items[item_id]
-        return {"message": "Item deleted."}
-    except KeyError:
-        abort(404, message="Item not found.")
-
-@app.delete("/store/<string:store_id>")
-def delete_store(store_id):
-    try:
-        del stores[store_id]
-        return {"message": "store deleted."}
-    except KeyError:
-        abort(404, message="store not found.")
 
 @app.put("/item/<string:item_id>")
 def update_item(item_id):
@@ -98,5 +91,13 @@ def update_item(item_id):
         item = items[item_id]
         item |= item_data
         return item
+    except KeyError:
+        abort(404, message="Item not found.")
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
     except KeyError:
         abort(404, message="Item not found.")
