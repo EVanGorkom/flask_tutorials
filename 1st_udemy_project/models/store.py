c2c629ebc9f691a6d85a73f331d0c2a6b9fba2d1
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-04-22 22:46:10 -0500

    Update the relationships between our models and then update our schema models to reflect the relationships created without looping

--- 1st_udemy_project/models/store.py
+++ 1st_udemy_project/models/store.py
@@ -5,3 +5,4 @@ class StoreModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
+    items = db.relationship("ItemModel", back_populate="store", lazy="dynamic")
